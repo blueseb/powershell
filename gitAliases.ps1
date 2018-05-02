4d537a5931a1fac2bf9ad937c4b27ed684fb8a1a
Sebastiano Lanza <lanzas@rsc.org>
2018-05-02 13:15:43 +0100

    added aliases for git tools

---
+++ gitAliases.ps1
@@ -0,0 +1,8 @@
+function gex {gitex}
+function gec {gitex commit}
+function ggu {git gui}
+function gma {git checkout master}
+function gpu {git pull --rebase}
+function gst {git status}
+Set-Alias gti git
+Set-Alias igt git
